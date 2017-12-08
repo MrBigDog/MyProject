381decd8cdf32330fd34e1c40fb700989a79ee12
MrBigDog <635669462@qq.com>
2017-12-08 17:48:28 +0800

    Add files via upload

---
+++ src/constants.h
@@ -0,0 +1,222 @@
+#ifndef constants_h__
+#define constants_h__
+
+//#include <compiler.h>
+#include <cmath>
+
+
+#ifndef M_PI
+#  define M_PI             ((double)3.141592653589793238462643)
+#endif
+#ifndef TWO_PI
+#  define TWO_PI           ((double)(2.0 * M_PI))
+#endif
+#ifndef DEG_PER_RAD
+#  define DEG_PER_RAD      ((double)(180.0/M_PI))
+#endif
+#ifndef SEC_PER_RAD
+#  define SEC_PER_RAD      ((double)206264.8062471)
+#endif
+#ifndef RAD_PER_DEG
+#  define RAD_PER_DEG      ((double)(M_PI/180.0))
+#endif
+#ifndef MTRS_PER_FT
+#  define MTRS_PER_FT      ((double)0.3048)
+#endif
+#ifndef FT_PER_MTRS
+#  define FT_PER_MTRS      ((double)3.280839895013122)
+#endif
+#ifndef FT_PER_MILE
+#  define FT_PER_MILE      ((double)5280.0)
+#endif
+#ifndef SQRT_TWO_PI
+#  define SQRT_TWO_PI      ((double)2.50662827463100050242)
+#endif
+#ifndef SQRT_TWO_PI_DIV2
+#  define SQRT_TWO_PI_DIV2 ((double)7.07106781186547524401E-1)
+#endif
+
+
+enum gwCoordSysOrientMode
+{
+	GW_LEFT_HANDED = 0,
+	GW_RIGHT_HANDED = 1
+};
+
+// Make sure PI is defined in its various forms
+#ifndef GWD_PI // remove me once GWEarth no longer uses PLIB
+
+#ifdef M_PI
+const double GWD_PI = M_PI;
+const float GW_PI = (float)M_PI;
+#else
+const float GW_PI = 3.1415926535f;
+const double GWD_PI = 3.1415926535;
+#endif
+
+#endif // of PLIB-SG guard
+
+/** 2 * PI */
+const double GWD_2PI = GWD_PI * 2.0;
+
+/** PI / 2 */
+#ifdef M_PI_2
+#  define  SGD_PI_2  M_PI_2
+#else
+#  define  SGD_PI_2  1.57079632679489661923
+#endif
+
+/** PI / 4 */
+const double SGD_PI_4 = 0.78539816339744830961;
+
+#ifndef SGD_DEGREES_TO_RADIANS // // remove me once GWEarth no longer uses PLIB
+
+const double SGD_DEGREES_TO_RADIANS = GWD_PI / 180.0;
+const double SGD_RADIANS_TO_DEGREES = 180.0 / GWD_PI;
+
+const float GW_DEGREES_TO_RADIANS = GW_PI / 180.0f;
+const float GW_RADIANS_TO_DEGREES = 180.0f / GW_PI;
+
+#endif // of PLIB-SG guard
+
+/** \def GW_E "e" */
+#ifdef M_E
+#  define GW_E     M_E
+#else
+#  define GW_E     2.7182818284590452354
+#endif
+
+/** pi/180/60/60, or about 100 feet at earths' equator */
+#define GW_ONE_SECOND 4.848136811E-6
+
+
+/** Radius of Earth in kilometers at the equator.  Another source had
+ *  6378.165 but this is probably close enough */
+#define GW_EARTH_RAD 6378.155
+
+ // Maximum terrain elevation from sea level
+#define GW_MAX_ELEVATION_M 9000.0
+
+// Earth parameters for WGS 84, taken from LaRCsim/ls_constants.h
+
+/** Value of earth radius from LaRCsim (ft) */
+#define GW_EQUATORIAL_RADIUS_FT  20925650.
+
+/** Value of earth radius from LaRCsim (meter) */
+#define GW_EQUATORIAL_RADIUS_M   6378138.12
+
+/** Radius squared (ft) */
+#define GW_EQ_RAD_SQUARE_FT 437882827922500.
+
+/** Radius squared (meter) */
+#define GW_EQ_RAD_SQUARE_M   40680645877797.1344
+
+
+// Physical Constants, SI
+
+/**mean gravity on earth */
+#define GW_g0_m_p_s2          9.80665   // m/s2
+
+/**standard pressure at SL */
+#define GW_p0_Pa              101325.0 // Pa
+
+/**standard density at SL */
+#define GW_rho0_kg_p_m3       1.225 // kg/m3
+
+/**standard temperature at SL */
+#define GW_T0_K               288.15   // K (=15degC)
+
+/**specific gas constant of air*/
+#define GW_R_m2_p_s2_p_K      287.05   // m2/s2/K
+
+/**specific heat constant at constant pressure*/
+#define GW_cp_m2_p_s2_p_K 1004.68      // m2/s2/K   
+
+/**ratio of specific heats of air*/
+#define GW_gamma              1.4         // =cp/cv (cp = 1004.68 m2/s2 K , cv = 717.63 m2/s2 K)
+
+/**constant beta used to calculate dynamic viscosity */
+#define GW_beta_kg_p_sm_sqrK  1.458e-06   // kg/s/m/SQRT(K) 
+
+/** Sutherland constant */
+#define GW_S_K                110.4       // K
+
+
+// Conversions
+
+/** Arc seconds to radians.  (arcsec*pi)/(3600*180) = rad */
+#define GW_ARCSEC_TO_RAD    4.84813681109535993589e-06 
+
+/** Radians to arc seconds.  (rad*3600*180)/pi = arcsec */
+#define GW_RAD_TO_ARCSEC    206264.806247096355156
+
+/** Feet to Meters */
+#define GW_FEET_TO_METER    0.3048
+
+/** Meters to Feet */
+#define GW_METER_TO_FEET    3.28083989501312335958  
+
+/** Meters to Nautical Miles.  1 nm = 6076.11549 feet */
+#define GW_METER_TO_NM      0.0005399568034557235
+
+/** Nautical Miles to Meters */
+#define GW_NM_TO_METER      1852.0000
+
+/** Meters to Statute Miles. */
+#define GW_METER_TO_SM      0.0006213699494949496
+
+/** Statute Miles to Meters. */
+#define GW_SM_TO_METER      1609.3412196
+
+/** Radians to Nautical Miles.  1 nm = 1/60 of a degree */
+#define GW_NM_TO_RAD        0.00029088820866572159
+
+/** Nautical Miles to Radians */
+#define GW_RAD_TO_NM        3437.7467707849392526
+
+/** meters per second to Knots */
+#define GW_MPS_TO_KT        1.9438444924406046432
+
+/** Knots to meters per second */
+#define GW_KT_TO_MPS        0.5144444444444444444
+
+/** Feet per second to Knots */
+#define GW_FPS_TO_KT        0.5924838012958962841
+
+/** Knots to Feet per second */
+#define GW_KT_TO_FPS        1.6878098571011956874
+
+/** meters per second to Miles per hour */
+#define GW_MPS_TO_MPH       2.2369362920544020312
+
+/** meetrs per hour to Miles per second */
+#define GW_MPH_TO_MPS       0.44704
+
+/** Meters per second to Kilometers per hour */
+#define GW_MPS_TO_KMH       3.6
+
+/** Kilometers per hour to meters per second */
+#define GW_KMH_TO_MPS       0.2777777777777777778
+
+/** Pascal to Inch Mercury */
+#define GW_PA_TO_INHG       0.0002952998330101010
+
+/** Inch Mercury to Pascal */
+#define GW_INHG_TO_PA       3386.388640341
+
+/** slug/ft3 to kg/m3 */
+#define GW_SLUGFT3_TO_KGPM3   515.379
+
+
+/** For divide by zero avoidance, this will be close enough to zero */
+#define GW_EPSILON 0.0000001
+
+/** Highest binobj format version we know how to read/write.  This starts at
+ *  0 and can go up to 65535 */
+#define GW_BINOBJ_VERSION 6
+
+ /** for backwards compatibility */
+#define GW_SCENERY_FILE_FORMAT "0.4"
+
+
+#endif // constants_h__
